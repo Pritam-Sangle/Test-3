cfe9b6b700eb52ea6cb047a3c22c24ebc19f6de2
unknown <sanglepm@gmail.com>
2022-05-17 09:59:16 +0530

    added thirsty code

--- hungry.py
+++ hungry.py
@@ -5,3 +5,11 @@ elif hungry == 'no':
     print("do your homework.")
 else:
     print("you are confused.")
+
+thirsty=input("Are you thirsty?")
+if thirsty == 'yes':
+    print("drink water.")
+elif thirsty == 'no':
+    print("do your work.")
+else:
+    print("you are confused.")
